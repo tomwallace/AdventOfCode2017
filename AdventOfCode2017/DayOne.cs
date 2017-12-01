87b73cf66d3f546a7c04efe7dc5b0502a292c5b0
Tom Wallace <thomaspwallace@gmail.com>
2017-12-01 08:20:49 -0500

    Adding Day One solutions.

---
+++ AdventOfCode2017/DayOne.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace AdventOfCode2017
+{
+    public class DayOne
+    {
+        public int PartA(string input)
+        {
+            int total = 0;
+
+            char[] split = input.ToCharArray();
+            int splitSize = split.Length;
+
+            for (int i = 0; i < splitSize; i++)
+            {
+                int convertedCurrent = Int32.Parse(split[i].ToString());
+                int convertedNext;
+                if (i == splitSize - 1)
+                    convertedNext = Int32.Parse(split[0].ToString());
+                else
+                    convertedNext = Int32.Parse(split[i + 1].ToString());
+
+                if (convertedCurrent == convertedNext)
+                    total += convertedCurrent;
+            }
+
+            return total;
+        }
+
+        public int PartB(string input)
+        {
+            int total = 0;
+
+            char[] split = input.ToCharArray();
+            int splitSize = split.Length;
+            int stepsForward = splitSize / 2;
+
+            for (int i = 0; i < splitSize; i++)
+            {
+                int convertedCurrent = Int32.Parse(split[i].ToString());
+
+                int convertedSteps = i + stepsForward;
+                if (convertedSteps > (splitSize - 1))
+                    convertedSteps = convertedSteps - splitSize;
+
+                int convertedNext = Int32.Parse(split[convertedSteps].ToString());
+
+                if (convertedCurrent == convertedNext)
+                    total += convertedCurrent;
+            }
+
+            return total;
+        }
+    }
+}
