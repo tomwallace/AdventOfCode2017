0f65bebc51a1231bb474ab06e93e88969830fdd2
Tom Wallace <thomaspwallace@gmail.com>
2017-12-05 08:08:54 -0500

    Adding solutions for Day 5 puzzles.

---
+++ AdventOfCode2017.Tests/DayFiveTests.cs
@@ -0,0 +1,46 @@
+﻿using AdventOfCode2017.Five;
+using Xunit;
+
+namespace AdventOfCode2017.Tests
+{
+    public class DayFiveTests
+    {
+        [Fact]
+        public void testCountNumberOfSteps()
+        {
+            int[] input = new[] {0, 3, 0, 1, -3};
+            var sut = new DayFive();
+            var result = sut.CountNumberOfSteps(input);
+
+            Assert.Equal(5, result);
+        }
+
+        [Fact]
+        public void testCountNumberOfStepsComplex()
+        {
+            int[] input = new[] { 0, 3, 0, 1, -3 };
+            var sut = new DayFive();
+            var result = sut.CountNumberOfStepsComplex(input);
+
+            Assert.Equal(10, result);
+        }
+
+        [Fact]
+        public void testPartA()
+        {
+            var sut = new DayFive();
+            var result = sut.PartA();
+
+            Assert.Equal("372671", result);
+        }
+
+        [Fact]
+        public void testPartB()
+        {
+            var sut = new DayFive();
+            var result = sut.PartB();
+
+            Assert.Equal("25608480", result);
+        }
+    }
+}
---
+++ AdventOfCode2017/Five/DayFive.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode2017.Five
+{
+    public class DayFive : IAdventProblemSet
+    {
+        public string Description()
+        {
+            return "A Maze of Twisty Trampolines, All Alike";
+        }
+
+        public int SortOrder()
+        {
+            return 5;
+        }
+
+        public string PartA()
+        {
+            int[] offsets = CollectOffsetsFromInput();
+            return CountNumberOfSteps(offsets).ToString();
+        }
+
+        public string PartB()
+        {
+            int[] offsets = CollectOffsetsFromInput();
+            return CountNumberOfStepsComplex(offsets).ToString();
+        }
+
+        public int CountNumberOfSteps(int[] offsets)
+        {
+            int pointer = 0;
+            int steps = 0;
+
+            while (pointer < offsets.Length)
+            {
+                int currentValue = offsets[pointer];
+                offsets[pointer]++;
+                pointer += currentValue;
+
+                steps++;
+            }
+
+            return steps;
+        }
+
+        public int CountNumberOfStepsComplex(int[] offsets)
+        {
+            int pointer = 0;
+            int steps = 0;
+
+            while (pointer < offsets.Length)
+            {
+                int currentValue = offsets[pointer];
+                if (currentValue >= 3)
+                    offsets[pointer]--;
+                else
+                    offsets[pointer]++;
+
+                pointer += currentValue;
+
+                steps++;
+            }
+
+            return steps;
+        }
+
+        private int[] CollectOffsetsFromInput()
+        {
+            string line;
+            StreamReader file = new StreamReader(@"Five\DayFiveInput.txt");
+
+            List<int> offsets = new List<int>();
+            // Iterate over each line in the input
+            while ((line = file.ReadLine()) != null)
+            {
+                offsets.Add(Int32.Parse(line));
+            }
+            file.Close();
+
+            return offsets.ToArray();
+        }
+    }
+}
