3761478e8625e06ce8be71629be983e913e5c20c
Tom Wallace <thomaspwallace@gmail.com>
2017-12-17 08:17:26 -0500

    Solution to Part A of DaySixteen.  Part B needs to be refactored as my existing solution did not finish overnight.

---
+++ AdventOfCode2017.Tests/DaySixteenTests.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using AdventOfCode2017.Sixteen;
+using Xunit;
+
+namespace AdventOfCode2017.Tests
+{
+    public class DaySixteenTests
+    {
+        [Theory]
+        [InlineData(1, "eabcd")]
+        [InlineData(2, "deabc")]
+        [InlineData(3, "cdeab")]
+        [InlineData(4, "bcdea")]
+        [InlineData(5, "abcde")]
+        public void TestSpin(int steps, string expected)
+        {
+            var sut = new Spin($"s{steps}");
+            var dancers = "abcde";
+            var result = sut.Execute(dancers);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void TestPartner()
+        {
+            var sut = new Partner("pe/b");
+            var dancers = "abcde";
+            var result = sut.Execute(dancers);
+
+            Assert.Equal("aecdb", result);
+        }
+
+        [Fact]
+        public void TestExchange()
+        {
+            var sut = new Exchange("x3/4");
+            var dancers = "abcde";
+            var result = sut.Execute(dancers);
+
+            Assert.Equal("abced", result);
+        }
+
+        [Fact]
+        public void TestExecuteDance()
+        {
+            var dancers = "abcde";
+            string danceMoves = "s1,x3/4,pe/b";
+            var sut = new DaySixteen();
+            var result = sut.ExecuteDance(dancers, danceMoves);
+
+            Assert.Equal("baedc", result);
+        }
+
+        [Fact]
+        public void TestPartA()
+        {
+            var sut = new DaySixteen();
+            var result = sut.PartA();
+
+            Assert.Equal("cgpfhdnambekjiol", result);
+        }
+
+        // TODO: I have to refactor this somehow, as the 1 billion iterations was only on 2 million after running overnight
+        /*
+        [Fact]
+        public void TestPartB()
+        {
+            var sut = new DaySixteen();
+            var result = sut.PartB();
+
+            Assert.Equal("", result);
+        }
+        */
+    }
+}
